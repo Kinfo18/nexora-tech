b231266427fe64e67ea97ece4092bdf8671ba3c8
Kinfo18 <davidfigueleo@gmail.com>
2026-03-18 11:47:06 -0500

    Proyecto creado y primera configuración: Tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://nexora.tech.com',
+    vite: {
+        // @ts-ignore - A veces necesario si los tipos de Tailwind v4 y Vite 6 no sincronizan perfecto
+        plugins: [tailwindcss()],
+    },
+});
